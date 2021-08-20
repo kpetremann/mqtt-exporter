17bf7e1e0306d8e2bf09d4b92dae9d51da87e872
Kevin Petremann <kpetrem@gmail.com>
2021-08-20 18:52:53 +0000

    Add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,29 @@
+"""Some tasks."""
+import os
+
+from invoke import task  # type: ignore
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+@task
+def install(cmd):
+    """Install virtualenv."""
+    cmd.run(f"python -m venv {PATH}/.venv")
+    with cmd.prefix(f"source {PATH}/.venv/bin/activate"):
+        cmd.run(f"pip install -r {PATH}/requirements/dev.txt")
+
+
+@task
+def reformat(cmd):
+    """Auto format using black and isort."""
+    with cmd.prefix(f"source {PATH}/.venv/bin/activate"):
+        cmd.run(f"isort {PATH}/")
+        cmd.run(f"black {PATH}/")
+
+
+@task
+def start(cmd):
+    """Start the app."""
+    with cmd.prefix(f"source {PATH}/.venv/bin/activate"):
+        cmd.run(f"python {PATH}/exporter.py")
