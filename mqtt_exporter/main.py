a38b18cff300d7859976e5288f778c80020ef485
Kevin Petremann <kpetrem@gmail.com>
2022-08-26 16:54:13 +0000

    Make mqtt message logging optional

--- mqtt_exporter/main.py
+++ mqtt_exporter/main.py
@@ -249,7 +249,8 @@ def expose_metrics(_, userdata, msg):
             LOG.debug('Topic "%s" was ignored by entry "%s"', msg.topic, ignore)
             return
 
-    logging.debug("New message from MQTT: %s - %s", msg.topic, msg.payload)
+    if settings.LOG_MQTT_MESSAGE:
+        LOG.debug("New message from MQTT: %s - %s", msg.topic, msg.payload)
 
     topic, payload = _parse_message(msg.topic, msg.payload)
 
